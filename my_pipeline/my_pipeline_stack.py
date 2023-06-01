139c67d7640374e59fbdc03f8e96c028e7afa39d
Sreshthak15 <sreshthak15@gmail.com>
2023-05-31 22:02:06 -0500

    Update my_pipeline_stack.py

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -13,7 +13,7 @@ class MyPipelineStack(cdk.Stack):
                             input=CodePipelineSource.git_hub("Sreshthak15/final", "main"),
                             commands=["echo '#!/bin/bash' > /codebuild/output/tmp/script.sh",
                                 "cat /codebuild/output/tmp/script.sh",
-                                "source .venv/bin/activate",
+                                ".venv/bin/activate",
                                 "python -m pip install -r requirements.txt",
                                 "cdk --version",
                                 "cdk synth"]

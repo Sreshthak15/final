56ca6e33d4fb61d1d812fde54591b1becbc47035
Sreshthak15 <sreshthak15@gmail.com>
2023-05-31 21:55:31 -0500

    Create my_pipeline_stack.py
    now

---
+++ my_pipeline/my_pipeline_stack.py
@@ -0,0 +1,21 @@
+import aws_cdk as cdk
+from constructs import Construct
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+
+class MyPipelineStack(cdk.Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        pipeline =  CodePipeline(self, "Pipeline",
+                        pipeline_name="MyPipeline",
+                        synth=ShellStep("Synth",
+                            input=CodePipelineSource.git_hub("Sreshthak15/final", "main"),
+                            commands=["echo '#!/bin/bash' > /codebuild/output/tmp/script.sh",
+                                "cat /codebuild/output/tmp/script.sh",
+                                "source .venv/bin/activate",
+                                "python -m pip install -r requirements.txt",
+                                "cdk --version",
+                                "cdk synth"]
+                        )
+                    )
